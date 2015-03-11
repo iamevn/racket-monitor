91add6bae4488d9deecc5516778aac56b93f30c2
iamevn <iamevn@users.noreply.github.com>
2015-03-10 19:25:01 -0700

    condition variables started, having issues with how macros interact with scope

--- monitor.rkt
+++ monitor.rkt
@@ -1,35 +1,65 @@
 #lang racket
 ; implements a synchronization montior
 (provide 
-  ; initialize the monitor (make-monitor (public p1 p2 ...) (define (p1) ...) (define (p2 arg1) ...) ...)
-  make-monitor 
-  ; call method in monitor synchronously (monitor-call monitor-name procedure-name [arg1 ...])
-  monitor-call)
+ ; initialize the monitor (make-monitor (public p1 p2 ...) (define (p1) ...) (define (p2 arg1) ...) ...)
+ make-monitor 
+ ; call method in monitor synchronously (monitor-call monitor-name procedure-name [arg1 ...])
+ monitor-call)
 (define-syntax make-monitor
   (syntax-rules ()
     ((_ body ...)
-     (new (class monitor% body ... (super-new))))))
+     (new (class monitor% 
+            (inherit make-cv cv-empty? cv-wait cv-signal cv-signal-all)
+            body ...
+            (super-new))))))
 
 (define-syntax monitor-call
   (syntax-rules ()
     ((_ mon proc ...)
      (begin 
-       (send mon monitor%-p)
+       (send mon monitor%-lock)
        (send mon proc ...)
-       (send mon monitor%-v)))))
+       (send mon monitor%-unlock)))))
 
 (define monitor%
   (class object%
-    (public monitor%-call monitor%-p monitor%-v)
+    (public monitor%-call monitor%-lock monitor%-unlock
+            make-cv cv-empty? cv-wait cv-signal cv-signal-all)
+    
     (define monitor%-guard (make-semaphore 1))
+    
     (define monitor%-call 
       (λ (f ...)
         (semaphore-wait monitor%-guard)
         (f ...)
         (semaphore-post monitor%-guard)))
-    (define monitor%-p
+    
+    (define monitor%-lock
       (λ () (semaphore-wait monitor%-guard)))
-    (define monitor%-v
+    
+    (define monitor%-unlock
       (λ () (semaphore-post monitor%-guard)))
+    
+    (define make-cv
+      (λ () (vector (make-semaphore) 0)))
+    
+    (define cv-empty?
+      (λ (cv) (zero? (vector-ref cv 1))))
+    
+    (define cv-wait
+      (λ (cv) (vector-set! cv 1 (add1 (vector-ref cv 1)))
+        (monitor%-unlock)
+        (semaphore-wait (vector-ref cv 0))
+        (vector-set! cv 1 (sub1 (vector-ref cv 1)))
+        (monitor%-lock)))
+    
+    (define cv-signal
+      (λ (cv) (unless (cv-empty? cv) (semaphore-post (vector-ref cv 0)))))
+    
+    (define cv-signal-all
+      (λ (cv) (let loop ([i (vector-ref cv 1)])
+                (unless (zero? i)
+                  (semaphore-post (vector-ref cv 0))
+                  (loop (sub1 i))))))
     
     (super-new)))
--- test2.rkt
+++ test2.rkt
@@ -1,32 +1,35 @@
-; doesn't run yet
-(define timer%
-  (class monitor%
-         (public delay tick)
+#lang racket
+#| I don't think I know how macros work exactly. This runs
+   if you explicitly (inherit make-cv cv-wait) inside the
+   monitor definition but the make-monitor macro which should
+   be inheriting these methods, does not actually inherit them |#
+(require "monitor.rkt")
 
-         (define check (make-cv))
-         (define tod 0)
-         (define min +inf.0)
-
-         (define (delay interval)
-           (set! min (min min (+ tod interval)))
-           (let loop ([wake_time (+ tod interval)])
-             (when (> wake_time tod)
-               (cv-wait check)
-               (loop wake_time))))
-
-         (define (tick)
-           (set! tod (add1 tod))
-           (when (>= tod min) (cv-signal-all check)))
-
-         (super-new)))
-(define timer (new timer%))
+(define timer
+  (make-monitor
+   (inherit make-cv cv-empty? cv-wait cv-signal cv-signal-all) ;why is this necessary? make-monitor should take care of this???
+   
+   (public delay tick)
+   
+   (define check (make-cv))
+   (define tod 0)
+   
+   (define (delay interval)
+     (let loop ([wake_time (+ tod interval)])
+       (when (> wake_time tod)
+         (cv-wait check)
+         (loop wake_time))))
+   
+   (define (tick)
+     (set! tod (add1 tod))
+     (cv-signal-all check))))
 
 (define clock (thread
-                (lambda ()
-                  (let loop ()
-                    (sleep 1)
-                    (monitor-call timer tick)
-                    (loop)))))
+               (lambda ()
+                 (let loop ()
+                   (sleep 1)
+                   (monitor-call timer tick)
+                   (loop)))))
 
 (display "see you in 5 seconds...")
 (monitor-call timer delay 5)
